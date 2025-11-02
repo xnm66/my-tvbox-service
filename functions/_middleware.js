1fee6e0fe6b00ebd1da7cf4fa045b17404f21711
小柠檬 <54431854+xnm66@users.noreply.github.com>
2025-11-03 02:08:56 +0800

    Update _middleware.js via web editor

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -21,7 +21,7 @@ export function onRequest(context) {
     const jsonData = {
       "urls": [
         {
-          "name": "🍎小苹果影视",
+          "name": "🍎小苹果",
           "url": url.origin + "/data.json"
         }
       ]
